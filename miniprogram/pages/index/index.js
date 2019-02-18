a926e91b6400ddf7f4f973b1c41a0b2f32969222
shuai <19861458@qq.com>
2019-02-18 15:59:00 +0800

    更改过的新版本2
    22222

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -4,22 +4,94 @@ const app = getApp()
 
 Page({
   data: {
-    motto: '你好，小程序',
-    tips: '这是一个DOME',
-    userInfo: {},
+    list_data:[],
+    winHeight: "",//窗口高度
+    currentTab: 0, //预设当前项的值
+    scrollLeft: 0, //tab标题的滚动条位置
+    expertList: [{ //假数据
+      img: "avatar.png",
+      name: "欢顔",
+      tag: "知名情感博主",
+      answer: 134,
+      listen: 2234
+    }],
     // 遮罩层
     showModal: false,
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    duration: 2000,
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    loading: false,
+    plain: false
   },
-
+  // 滚动切换标签样式
+  switchTab: function (e) {
+    this.setData({
+      currentTab: e.detail.current
+    });
+    this.checkCor();
+  },
+  // 点击标题切换当前页时改变样式
+  swichNav: function (e) {
+    var cur = e.target.dataset.current;
+    if (this.data.currentTaB == cur) { return false; }
+    else {
+      this.setData({
+        currentTab: cur
+      })
+    }
+  },
+  //判断当前滚动超过一屏时，设置tab标题滚动条。
+  checkCor: function () {
+    if (this.data.currentTab > 4) {
+      this.setData({
+        scrollLeft: 300
+      })
+    } else {
+      this.setData({
+        scrollLeft: 0
+      })
+    }
+  },
   //事件处理函数
   bindViewTap: function () {
-    wx.navigateTo({
-      url: '../logs/logs'
+    wx.switchTab({    //跳转到tabBar页面，并关闭其他所有tabBar页面
+      // url: "/pages/listb/listb"
     })
   },
-  onLoad: function () {
+  onLoad: function (options) {
+    var that = this//不要漏了这句，很重要
+    wx.request({
+      url: 'https://easy-mock.com/mock/5adc3ba46b6af920eaf26380/json',
+      // url:'http://m.lingdie.com/index.php?g=User&m=Baozhang&a=hahah',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      success: function (res) {
+        //将获取到的json数据，存在名字叫zhihu的这个数组中
+        that.setData({
+          list_data: res.data.list,
+          //res代表success函数的事件对，data是固定的，stories是是上面json数据中stories
+
+        })
+        console.log(res)
+      }
+    }),
+      //  高度自适应
+      wx.getSystemInfo({
+        success: function (res) {
+          var clientHeight = res.windowHeight,
+            clientWidth = res.windowWidth,
+            rpxR = 750 / clientWidth;
+          var calc = clientHeight * rpxR - 180;
+          console.log(calc)
+          that.setData({
+            winHeight: calc
+          });
+        }
+      });
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
@@ -47,6 +119,7 @@ Page({
       })
     }
   },
+  footerTap: app.footerTap,
   getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
@@ -68,7 +141,5 @@ Page({
     this.setData({
       showModal: false
     })
-  }
-
-
+  },
 })
